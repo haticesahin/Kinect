0a8e2d13237d62c59f3c2a00348fcf756a06a907
haticesahin <56666399+haticesahin@users.noreply.github.com>
2019-10-26 03:47:02 +0300

    Initial commit

---
+++ Assets/Scripts/TopKontrol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    Rigidbody fizik;
+    public int hiz;
+    int sayac = 0;
+    public int toplanilacakObjeSayisi;
+    public Text sayacText;
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+
+        fizik.AddForce(vec*hiz);
+        //Debug.Log("yatay = "+yatay+"       dikey ="+dikey);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Engel")
+        {
+            other.gameObject.SetActive(false);
+            sayac++;
+
+            sayacText.text = "Sayac =" + sayac;
+
+      //  if (sayac==toplanilacakObjeSayisi)
+      //  {
+      //      
+      //  }
+
+        }
+    }
+}
